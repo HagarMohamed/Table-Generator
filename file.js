216ba4d62f714093937c0bb6bc14b4f01fcfb166
unknown <hagarabdo7@gmail.com>
2022-05-03 00:56:48 +0200

    make html page with generate table with java script

---
+++ file.js
@@ -0,0 +1,52 @@
+function generate(e){
+    number_rows = document.getElementById('number_row').value;
+    number_columns = document.getElementById('number_column').value;
+
+
+    makeTableVisable();
+
+   tbody = document.getElementsByTagName('tbody')[0];
+  
+   clearTbodyChild(tbody);
+   
+   
+
+
+    for(let i=0; i< number_rows; i++){
+        tr = document.createElement("tr");
+        tbody.appendChild(tr);
+
+        for(let j=0; j< number_columns; j++){
+            td = document.createElement("td");
+            td.innerText = `${i},${j}`;
+            tr.appendChild(td);
+        }
+
+    }
+
+    resetInput();
+}
+
+
+function makeTableVisable(){
+    table = document.getElementsByTagName('table')[0];
+    table.style.visibility = "visible";
+}
+
+function resetInput(){
+    
+   document.getElementById('number_row').value = "";
+   document.getElementById('number_column').value = "";
+
+
+    
+}
+
+function clearTbodyChild(tbody){
+    // tbody.innerHTML = '';
+    while(tbody.firstChild){
+        tbody.removeChild(tbody.firstChild);
+    }
+
+
+}
