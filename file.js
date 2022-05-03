e50a22da89bb2ebf5627ac4e8b97eea62ddf2a40
unknown <hagarabdo7@gmail.com>
2022-05-03 17:44:33 +0200

    make table display random value

--- file.js
+++ file.js
@@ -1,24 +1,21 @@
-function generate(e){
+function generate(e) {
     number_rows = document.getElementById('number_row').value;
     number_columns = document.getElementById('number_column').value;
 
 
     makeTableVisable();
 
-   tbody = document.getElementsByTagName('tbody')[0];
-  
-   clearTbodyChild(tbody);
-   
-   
+    tbody = document.getElementsByTagName('tbody')[0];
+    clearTbodyChild(tbody);
 
-
-    for(let i=0; i< number_rows; i++){
+    for (let i = 0; i < number_rows; i++) {
         tr = document.createElement("tr");
         tbody.appendChild(tr);
 
-        for(let j=0; j< number_columns; j++){
+        for (let j = 0; j < number_columns; j++) {
             td = document.createElement("td");
-            td.innerText = `${i},${j}`;
+            // td.innerText = `${i},${j}`
+            td.innerText = generateRandomValue();
             tr.appendChild(td);
         }
 
@@ -28,25 +25,31 @@ function generate(e){
 }
 
 
-function makeTableVisable(){
+function makeTableVisable() {
     table = document.getElementsByTagName('table')[0];
     table.style.visibility = "visible";
 }
 
-function resetInput(){
-    
-   document.getElementById('number_row').value = "";
-   document.getElementById('number_column').value = "";
+function resetInput() {
+
+    document.getElementById('number_row').value = "";
+    document.getElementById('number_column').value = "";
 
 
-    
+
 }
 
-function clearTbodyChild(tbody){
+function clearTbodyChild(tbody) {
     // tbody.innerHTML = '';
-    while(tbody.firstChild){
+    while (tbody.firstChild) {
         tbody.removeChild(tbody.firstChild);
     }
 
 
+}
+
+function generateRandomValue() {
+    min = 0;
+    max = 100;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
